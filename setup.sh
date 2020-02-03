bbd667fff428be8eb49d5dbf8cbb51668cb3b483
brightcolor <brightcolor@users.noreply.github.com>
2020-02-03 18:23:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,8 +20,8 @@ echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/
 #echo "deb http://packages.dotdeb.org jessie-nginx-http2 all" >> /etc/apt/sources.list
 #echo "deb-src http://packages.dotdeb.org jessie-nginx-http2 all" >> /etc/apt/sources.list
 
-sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+#sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+#echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
 
 
 aptitude update
@@ -32,22 +32,22 @@ aptitude install -y lsb-release figlet python-apt mc iotop htop iftop bash-compl
 mkdir /etc/update-motd.d/
 cd /etc/update-motd.d/
 
-#wget --no-check-certificate -O /etc/update-motd.d/00-header https://raw.githubusercontent.com/brightcolor/server-init-setup/master/files/00-header
-#wget --no-check-certificate -O /etc/update-motd.d/10-sysinfo https://raw.githubusercontent.com/brightcolor/server-init-setup/master/files/10-sysinfo
-#wget --no-check-certificate -O /etc/update-motd.d/20-updates https://raw.githubusercontent.com/brightcolor/server-init-setup/master/files/20-updates
-#wget --no-check-certificate -O /etc/update-motd.d/90-footer https://raw.githubusercontent.com/brightcolor/server-init-setup/master/files/90-footer
-#wget -O /etc/update-motd.d/nancyj.flf http://www.figlet.org/fonts/nancyj.flf
+wget --no-check-certificate -O /etc/update-motd.d/00-header https://raw.githubusercontent.com/brightcolor/server-init-setup/master/files/00-header
+wget --no-check-certificate -O /etc/update-motd.d/10-sysinfo https://raw.githubusercontent.com/brightcolor/server-init-setup/master/files/10-sysinfo
+wget --no-check-certificate -O /etc/update-motd.d/20-updates https://raw.githubusercontent.com/brightcolor/server-init-setup/master/files/20-updates
+wget --no-check-certificate -O /etc/update-motd.d/90-footer https://raw.githubusercontent.com/brightcolor/server-init-setup/master/files/90-footer
+wget -O /etc/update-motd.d/nancyj.flf http://www.figlet.org/fonts/nancyj.flf
 
-#chmod +x /etc/update-motd.d/*
-#rm /etc/motd
-#ln -s /var/run/motd /etc/motd
+chmod +x /etc/update-motd.d/*
+rm /etc/motd
+ln -s /var/run/motd /etc/motd
 
 update-command-not-found
 
 wget --no-check-certificate -O /root/.bash_aliases https://raw.githubusercontent.com/brightcolor/server-init-setup/master/files/.bash_aliases
 rm /root/.bashrc
 wget --no-check-certificate -O /root/.bashrc https://raw.githubusercontent.com/brightcolor/server-init-setup/master/files/.bashrc
-#source /root/.bashrc
+source /root/.bashrc
 
 #mv /etc/postfix/smtpd.cert /etc/postfix/smtpd.cert-bak
 #mv /etc/postfix/smtpd.key /etc/postfix/smtpd.key-bak
